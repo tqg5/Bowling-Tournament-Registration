33b41c31d02dea235d08b90ac0ba6920c92aff0a
Trey Gaines <treyqg15@gmail.com>
2021-09-11 14:51:56 -0400

    fixed states.json import typescript issue

--- src/components/Form/State/index.tsx
+++ src/components/Form/State/index.tsx
@@ -1,19 +1,22 @@
 import { FC, useRef, ReactElement } from 'react';
 import { Select } from 'theme-ui';
 import { useDispatch } from 'react-redux';
-import states from './states';
+import * as states from './states.json';
 import constants from '../constants';
 import { saveState } from 'store/form/slice';
 
 export interface StateType {
-    key: String;
-    value: String;
+    key: string;
+    value: string;
 };
+
+type statesSchema = typeof import('./states.json');
 
 const createStatesArray = (): ReactElement[] => {
     const map: ReactElement[] = [];
-    
-    for(const key in states) {
+
+    let key: keyof typeof states;
+    for(key in states as statesSchema) {
         map.push(<option value={key}>{states[key]}</option>);
     };
 
@@ -33,7 +36,7 @@ const State: FC = () => {
             onChange={({ target: { value: key }}) => {
                 const obj = {
                     key,
-                    value: states[key]
+                    value: states[key as keyof typeof states]
                 }
 
                 dispatch(saveState(obj));
